e57bde9805bc74e4398360b503f72c7773745a60
YahyaaU <glvd0689@leeds.ac.uk>
2026-02-13 12:49:06 +0000

    Session 2

--- session-2/task1/sizes.c
+++ session-2/task1/sizes.c
@@ -11,10 +11,15 @@ int main( void ) {
     char testChar;
     int testInt;
     float testFloat;
+    short testShort;
+    long testLong;
+    double doubleFloat;
 
     printf("Char size is %ld bytes\n",sizeof(testChar));
     printf("Int size is %ld bytes\n",sizeof(testInt));
     printf("Float size is %ld bytes\n",sizeof(testFloat));
-
+    printf("Short Integer size is %ld bytes\n",sizeof(testShort));
+    printf("Long integer size is %ld bytes\n", sizeof(testLong));
+    printf("Double size is %ld bytes\n",sizeof(doubleFloat));
     return 0;
 }
--- session-2/task2/ptr_ops.c
+++ session-2/task2/ptr_ops.c
@@ -9,15 +9,15 @@
 int main( void ) {
 
     float testFloat = 3.14;
-    float *testFloatPtr;
+    float* testFloatPtr; // this is a pointer which points to a memory address
 
     testFloatPtr = &testFloat;  // the float pointer (testFloatPtr) is set to the address of the float variable (testFloat)
     
     printf("The value of the float variable is  %f\n",testFloat);
-    printf("The address of the float variable is  %p\n",&testFloat);
+    printf("The address of the float variable is %p \n",&testFloat); //the address of the variable in memory
 
-    printf("The value of the pointer is  %p\n",testFloatPtr);
-    printf("The value that the pointer dereferences to is  %f\n",*testFloatPtr);
+    printf("The value of the pointer is  %p\n",testFloatPtr); // pointer value at memory address...
+    printf("The value that the pointer dereferences to is  %f\n",*testFloatPtr); // get our integer variable
 
     return 0;
 }
