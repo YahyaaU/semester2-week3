cc10a14b9187cb0999e0d55c640ed798b4fbd6ba
YahyaaU <glvd0689@leeds.ac.uk>
2026-02-15 14:22:13 +0000

    Completed Quadratic Task

--- session-2/task3/quadratic.c
+++ session-2/task3/quadratic.c
@@ -11,17 +11,26 @@
 
 int main( void ) {
 
-    float a = 1.0;
-    float b = -5.0;
-    float c = 6.0;
-    float root1, root2;
+    float a = 1.0; // a = Co-efficient of x^2
+    float b = -5.0; // b = Co-efficient of x
+    float c = 6.0; // c = constant
+    float root1, root2; // defines 2 variables root1 and root2
+    float discriminant = b*b - 4*a*c; // b^2 - 4ac = discriminant
+    float sqrtX = sqrt(discriminant); // square roots the discriminant using the 'sqrt' function from the math library
+
+    root1 = (-b + sqrtX) / (2*a); // Computes the first root
+    root2 = (-b - sqrtX) / (2*a); // Computes the second root
+
+    printf("Root 1: %f\n", root1); // prints root1
+    printf("Root 2: %f\n", root2); // prints root2
+
 
     /*
      * Implement the formula for the 2 roots of a quadratic.
      * You can define additional variable for intermediate results to make the code simpler.
      * Print out the final results for the 2 roots as float values.
      */
-
+    // 
     
 
     return 0;
